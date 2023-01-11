15589a6d089401ef73989e068ab3a969bbe252a2
jmoush <122463077+jmoush@users.noreply.github.com>
2023-01-11 10:07:53 -0500

    Add files via upload

---
+++ PA 6-2022 Slave/main.d
@@ -0,0 +1,6 @@
+.\main.o: main.c
+.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\main.o: tm4c123gh6pm.h
+.\main.o: PLL.h
+.\main.o: UART.h
